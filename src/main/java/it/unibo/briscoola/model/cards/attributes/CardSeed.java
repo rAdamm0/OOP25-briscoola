c5c83ffe473f239cc78b54a64c33d9a78b5caafa
Andrea Reggiani <andrea.reggiani14@studio.unibo.it>
2026-03-17 15:42:52 +0100

    briscoola/model/cards: Created interface Card, and enum CardSeed and CardValue

---
+++ src/main/java/it/unibo/briscoola/model/cards/attributes/CardSeed.java
@@ -0,0 +1,24 @@
+package it.unibo.briscoola.model.cards.attributes;
+
+public enum CardSeed {
+    
+    /*  
+    *THE SYMBOL SWORD 
+    */
+    SWORD,
+
+    /*
+    *THE SYMBOL COIN 
+    */ 
+    COIN,
+
+    /*
+    *THE SYMBOL STAFF 
+    */
+    STAFF, 
+    
+    /*
+    *THE SYMBOL CUP 
+    */
+    CUP;
+}
---
+++ src/main/java/it/unibo/briscoola/model/cards/impl/StandardCardImpl.java
@@ -0,0 +1,5 @@
+package it.unibo.briscoola.model.cards.impl;
+
+public class StandardCardImpl {
+    
+}
