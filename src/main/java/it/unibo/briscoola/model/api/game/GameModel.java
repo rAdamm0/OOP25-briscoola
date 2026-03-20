212daf69dc5e556d3aefd724552ada05075804b0
maisam <maisam.noumi@studio.unibo.it>
2026-03-20 20:20:49 +0100

    model/api/game: add assignBriscola, dealInitialCards and drawAfterTrick to GameModel

--- src/main/java/it/unibo/briscoola/model/api/game/GameModel.java
+++ src/main/java/it/unibo/briscoola/model/api/game/GameModel.java
@@ -3,7 +3,7 @@ package it.unibo.briscoola.model.api.game;
 import java.util.Optional;
 
 import it.unibo.briscoola.model.api.card.Card;
-
+import it.unibo.briscoola.model.api.player.Player;
 public interface GameModel {
 
     /**
@@ -25,4 +25,23 @@ public interface GameModel {
       * When players have the Hand empty.
       */
      boolean isGameOver();
+
+    /**
+     * Picks the first card from the deck -> that will be the briscola
+     * and put it at the bottom of the deck.
+     */
+    void assignBriscola();
+
+    /**
+     * At first, deals 3 cards to each player, alternating between them
+     */
+    void dealInitialCards();
+
+    /**
+     * After a trick, the winner draws first, then the loser. 
+     * If the deck is empty, nobody will draw.
+     * @param winner the player who won the trick
+     * @param loser the player who lost the trick
+     */
+    void drawAfterTrick(Player winner, Player loser);
 }
