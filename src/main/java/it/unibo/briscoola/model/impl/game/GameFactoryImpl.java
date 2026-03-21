ef12138f3fc0b1ac522212256adc450502183034
Andrea Reggiani <andrea.reggiani14@studio.unibo.it>
2026-03-21 17:36:37 +0100

    started in model/impl/game/ : GameFactoryImpl and GameModelImpl

---
+++ src/main/java/it/unibo/briscoola/model/impl/game/GameFactoryImpl.java
@@ -0,0 +1,26 @@
+package it.unibo.briscoola.model.impl;
+
+import java.util.List;
+
+import it.unibo.briscoola.model.api.card.Card;
+import it.unibo.briscoola.model.api.deck.Deck;
+import it.unibo.briscoola.model.api.game.GameFactory;
+import it.unibo.briscoola.model.api.game.GameModel;
+import it.unibo.briscoola.model.api.player.Player;
+import it.unibo.briscoola.model.impl.deck.DeckImpl;
+
+public class GameFactoryImpl implements GameFactory {
+
+    /**
+     * {@inheritDoc}
+     */
+    
+    @Override
+    public GameModel createStandardGame(List<Player> players) {
+        Deck<Card> deck= new DeckImpl();
+
+        return new GameModelImpl(players,deck);
+    }
+
+    
+}
---
+++ src/main/java/it/unibo/briscoola/model/impl/game/GameModelImpl.java
@@ -0,0 +1,53 @@
+package it.unibo.briscoola.model.impl;
+
+import java.util.Optional;
+
+import it.unibo.briscoola.model.api.card.Card;
+import it.unibo.briscoola.model.api.game.GameModel;
+import it.unibo.briscoola.model.api.player.Player;
+
+public class GameModelImpl implements GameModel{
+
+    @Override
+    public void startMatch() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startMatch'");
+    }
+
+    @Override
+    public Optional<Card> getBriscolaSeed() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBriscolaSeed'");
+    }
+
+    @Override
+    public boolean isGameOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isGameOver'");
+    }
+
+    @Override
+    public void assignBriscola() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assignBriscola'");
+    }
+
+    @Override
+    public void dealInitialCards() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dealInitialCards'");
+    }
+
+    @Override
+    public void drawAfterTrick(Player winner, Player loser) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'drawAfterTrick'");
+    }
+
+    @Override
+    public Card playCard(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'playCard'");
+    }
+    
+}
