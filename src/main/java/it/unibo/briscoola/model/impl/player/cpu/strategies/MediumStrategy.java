89a3f3fd48c603be0a4fd2d43fc9ba503da157f8
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2026-05-13 18:17:18 +0200

    impl: Implemented the MediumStrategy

--- src/main/java/it/unibo/briscoola/model/impl/player/cpu/strategies/MediumStrategy.java
+++ src/main/java/it/unibo/briscoola/model/impl/player/cpu/strategies/MediumStrategy.java
@@ -3,8 +3,13 @@ package it.unibo.briscoola.model.impl.player.cpu.strategies;
 import it.unibo.briscoola.model.api.card.Card;
 import it.unibo.briscoola.model.api.player.PlayStrategy;
 import it.unibo.briscoola.model.impl.game.RoundStateImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Strategy of the CPU for choosing the card to be played in Medium Difficulty.
@@ -12,11 +17,18 @@ import java.util.List;
  */
 public class MediumStrategy implements PlayStrategy {
 
+    private final Logger logger = LoggerFactory.getLogger(MediumStrategy.class);
     /**
      * {@inheritDoc}
      */
     @Override
     public int cardIndex(final List<Card> hand, RoundStateImpl state) {
-        return 0;
+        Optional<Card> canBeat = hand.stream()
+                .filter(card ->
+                        state.playedCards().stream()
+                                .allMatch(it->
+                                        card.getCardPower() > it.card().getCardPower()))
+                .limit(1).findFirst();
+        return canBeat.map(hand::indexOf).orElseGet(() -> hand.indexOf(hand.stream().min(Comparator.comparingInt(Card::getCardPower)).orElseThrow()));
     }
 }
