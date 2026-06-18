f833a7989c72dcd4cb1af807e39a95c54356052f
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2026-06-18 15:34:00 +0200

    impl: Made a first implementation of the roundWinnerPopup and endGamePopup to test

--- src/main/java/it/unibo/briscoola/view/impl/popup/PopupFactoryImpl.java
+++ src/main/java/it/unibo/briscoola/view/impl/popup/PopupFactoryImpl.java
@@ -4,6 +4,7 @@ import it.unibo.briscoola.view.api.popup.PopupFactory;
 import it.unibo.briscoola.view.api.popup.Popups;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PopupFactoryImpl implements PopupFactory {
 
@@ -24,15 +25,23 @@ public class PopupFactoryImpl implements PopupFactory {
     }
 
     private Popup roundWinnerPopup(final JRootPane root, final String message){
+        String trophy = new String(Character.toChars(1096));
+        JComponent component = new JPanel(new GridLayout(0,1));
+        component.add(new JLabel(trophy));
+        component.add(new JLabel(message));
         return javax.swing.PopupFactory.getSharedInstance().getPopup(
                 root,
-                null,
+                component,
                 0,
                 0
         );
     }
 
     private Popup endGamePopup(final JRootPane root, final String message){
+        String trophy = new String(Character.toChars(1098));
+        JComponent component = new JPanel(new GridLayout(0,1));
+        component.add(new JLabel(trophy));
+        component.add(new JLabel(message));
         return javax.swing.PopupFactory.getSharedInstance().getPopup(
                 root,
                 null,
