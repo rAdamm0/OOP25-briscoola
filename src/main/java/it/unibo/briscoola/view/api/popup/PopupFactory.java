1c93a43bc069f330f6e273f9e0e0bf1247c1730f
Razzino Adam Paolo <adampaolo.razzino@studio.unibo.it>
2026-06-18 14:47:40 +0200

    api-impl: Created a Factory to deploy the requested popups

--- src/main/java/it/unibo/briscoola/view/api/popup/PopupFactory.java
+++ src/main/java/it/unibo/briscoola/view/api/popup/PopupFactory.java
@@ -1,4 +1,13 @@
 package it.unibo.briscoola.view.api.popup;
 
 public interface PopupFactory {
+
+    /**
+     * Based on the enum element of type {@link Popups} deploys the correct JPanel.
+     *
+     * @param popup of enum {@link Popups}
+     * @param message is a {@link String} to show on the popup
+     * @return a {@link PopUp} element
+     */
+    PopUp create (Popups popup, String message);
 }
---
+++ src/main/java/it/unibo/briscoola/view/impl/popup/PopupFactoryImpl.java
@@ -0,0 +1,16 @@
+package it.unibo.briscoola.view.impl.popup;
+
+import it.unibo.briscoola.view.api.popup.PopUp;
+import it.unibo.briscoola.view.api.popup.PopupFactory;
+import it.unibo.briscoola.view.api.popup.Popups;
+
+public class PopupFactoryImpl implements PopupFactory {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PopUp create(Popups popup, String message) {
+        return null;
+    }
+}
