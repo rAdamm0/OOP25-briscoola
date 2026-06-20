6b4269604ef0e9e3b62e2123d054558e7b7b5866
Andrea Reggiani <andrea.reggiani14@studio.unibo.it>
2026-06-20 18:45:09 +0200

    minor fix

--- src/main/java/it/unibo/briscoola/controller/impl/GameControllerImpl.java
+++ src/main/java/it/unibo/briscoola/controller/impl/GameControllerImpl.java
@@ -117,7 +117,7 @@ public class GameControllerImpl implements GameController {
                         view.updatePile(this.cpuPlayer.getPile().size(), false);
                     }
 
-                    view.updateTable(null, null, null, null); 
+                    view.updateTable(null, null, null, null,  this.model.getDeckSize()); 
                     updateAllHands();
                     manageTurn();
                 });
@@ -204,7 +204,7 @@ public class GameControllerImpl implements GameController {
             cValue = card.getCardValue().name();
         }
 
-        view.updateTable(pSeed, pValue, cSeed, cValue);
+        view.updateTable(pSeed, pValue, cSeed, cValue, this.model.getDeckSize());
     }
 
     private void updateAllHands() {
